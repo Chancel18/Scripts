9d7abbb55c868e16e11213991ee86633441a1f36
Gedeon Makela <chan.makela@gmail.com>
2022-02-16 15:59:10 +0100

    Add script batch and sql

---
+++ sql/disconnectUserDb.sql
@@ -0,0 +1,21 @@
+declare @sql as varchar(20), @spid as int
+
+select @spid = min(spid)  from master..sysprocesses  where dbid = db_id('AOGC_PROD') 
+and spid != @@spid    
+
+while (@spid is not null)
+begin
+    print 'Killing process ' + cast(@spid as varchar) + ' ...'
+    set @sql = 'kill ' + cast(@spid as varchar)
+    exec (@sql)
+
+    select 
+        @spid = min(spid)  
+    from 
+        master..sysprocesses  
+    where 
+        dbid = db_id('AOGC_PROD') 
+        and spid != @@spid
+end 
+
+print 'Process completed...'
